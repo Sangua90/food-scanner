da1f1145ab901113c523490e5dee7925d0e6cd31
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-08-25 10:28:14 +0200

    Use OptionsFlowWithReload without update listener

--- custom_components/food_scanner/__init__.py
+++ custom_components/food_scanner/__init__.py
@@ -13,7 +13,6 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     await async_setup_services(hass)
     return True
 
@@ -21,7 +20,3 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.get(DOMAIN, {}).pop(entry.entry_id, None)
     return True
-
-
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    await hass.config_entries.async_reload(entry.entry_id)
