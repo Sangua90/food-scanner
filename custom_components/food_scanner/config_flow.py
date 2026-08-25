c0c459474781b86f0d28a58a95a9f1939bf53f86
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-08-25 14:52:41 +0200

    Use HomeStock title for new config entries

--- custom_components/food_scanner/config_flow.py
+++ custom_components/food_scanner/config_flow.py
@@ -25,14 +25,11 @@ class FoodScannerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Food Scanner", data=user_input)
-
-        schema = vol.Schema(
-            {
-                vol.Required(CONF_API_KEY): str,
-                vol.Optional(CONF_MODEL, default=DEFAULT_MODEL): str,
-            }
-        )
+            return self.async_create_entry(title="HomeStock", data=user_input)
+        schema = vol.Schema({
+            vol.Required(CONF_API_KEY): str,
+            vol.Optional(CONF_MODEL, default=DEFAULT_MODEL): str,
+        })
         return self.async_show_form(step_id="user", data_schema=schema)
 
     @staticmethod
@@ -45,35 +42,16 @@ class FoodScannerOptionsFlow(config_entries.OptionsFlowWithReload):
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
-
-        current_model = self.config_entry.options.get(
-            CONF_MODEL,
-            self.config_entry.data.get(CONF_MODEL, DEFAULT_MODEL),
-        )
+        current_model = self.config_entry.options.get(CONF_MODEL, self.config_entry.data.get(CONF_MODEL, DEFAULT_MODEL))
         current_notify = self.config_entry.options.get(CONF_NOTIFY, DEFAULT_NOTIFY)
-        current_expiry_notify = self.config_entry.options.get(
-            CONF_EXPIRY_NOTIFY, DEFAULT_EXPIRY_NOTIFY
-        )
-        current_expiry_days = self.config_entry.options.get(
-            CONF_EXPIRY_NOTIFY_DAYS, DEFAULT_EXPIRY_NOTIFY_DAYS
-        )
-        current_notify_service = self.config_entry.options.get(
-            CONF_EXPIRY_NOTIFY_SERVICE, DEFAULT_EXPIRY_NOTIFY_SERVICE
-        )
-
-        schema = vol.Schema(
-            {
-                vol.Optional(CONF_MODEL, default=current_model): str,
-                vol.Optional(CONF_NOTIFY, default=current_notify): bool,
-                vol.Optional(CONF_EXPIRY_NOTIFY, default=current_expiry_notify): bool,
-                vol.Optional(
-                    CONF_EXPIRY_NOTIFY_DAYS,
-                    default=current_expiry_days,
-                ): vol.All(vol.Coerce(int), vol.Range(min=0, max=365)),
-                vol.Optional(
-                    CONF_EXPIRY_NOTIFY_SERVICE,
-                    default=current_notify_service,
-                ): str,
-            }
-        )
+        current_expiry_notify = self.config_entry.options.get(CONF_EXPIRY_NOTIFY, DEFAULT_EXPIRY_NOTIFY)
+        current_expiry_days = self.config_entry.options.get(CONF_EXPIRY_NOTIFY_DAYS, DEFAULT_EXPIRY_NOTIFY_DAYS)
+        current_notify_service = self.config_entry.options.get(CONF_EXPIRY_NOTIFY_SERVICE, DEFAULT_EXPIRY_NOTIFY_SERVICE)
+        schema = vol.Schema({
+            vol.Optional(CONF_MODEL, default=current_model): str,
+            vol.Optional(CONF_NOTIFY, default=current_notify): bool,
+            vol.Optional(CONF_EXPIRY_NOTIFY, default=current_expiry_notify): bool,
+            vol.Optional(CONF_EXPIRY_NOTIFY_DAYS, default=current_expiry_days): vol.All(vol.Coerce(int), vol.Range(min=0, max=365)),
+            vol.Optional(CONF_EXPIRY_NOTIFY_SERVICE, default=current_notify_service): str,
+        })
         return self.async_show_form(step_id="init", data_schema=schema)
