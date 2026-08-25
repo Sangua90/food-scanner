a9fe6a993d3f3a4252802fcf0d13c7095b87e309
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-08-25 10:19:45 +0200

    Add Food Scanner constants

---
+++ custom_components/food_scanner/const.py
@@ -0,0 +1,7 @@
+DOMAIN = "food_scanner"
+CONF_API_KEY = "api_key"
+CONF_MODEL = "model"
+CONF_NOTIFY = "notify"
+DEFAULT_MODEL = "gemini-3.5-flash-lite"
+DEFAULT_NOTIFY = True
+SERVICE_SCAN_IMAGE = "scan_image"
