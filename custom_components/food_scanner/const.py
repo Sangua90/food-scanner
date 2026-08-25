741f00c96a0416b17eb06fea8ed3dcec04cd5aba
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-08-25 11:37:18 +0200

    Add inventory and expiry notification constants

--- custom_components/food_scanner/const.py
+++ custom_components/food_scanner/const.py
@@ -1,7 +1,19 @@
 DOMAIN = "food_scanner"
+
 CONF_API_KEY = "api_key"
 CONF_MODEL = "model"
 CONF_NOTIFY = "notify"
+CONF_EXPIRY_NOTIFY = "expiry_notify"
+CONF_EXPIRY_NOTIFY_DAYS = "expiry_notify_days"
+CONF_EXPIRY_NOTIFY_SERVICE = "expiry_notify_service"
+
 DEFAULT_MODEL = "gemini-3.5-flash-lite"
 DEFAULT_NOTIFY = True
+DEFAULT_EXPIRY_NOTIFY = True
+DEFAULT_EXPIRY_NOTIFY_DAYS = 3
+DEFAULT_EXPIRY_NOTIFY_SERVICE = ""
+
 SERVICE_SCAN_IMAGE = "scan_image"
+SERVICE_CONSUME_PRODUCT = "consume_product"
+SERVICE_REMOVE_PRODUCT = "remove_product"
+SERVICE_CLEAR_ARCHIVE = "clear_archive"
