cf6940d3e8f8c4d1d144e6970ce2ed6a3f0de389
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-09-05 14:13:57 +0200

    Align HomeStock panel version 1.6.5

--- custom_components/food_scanner/__init__.py
+++ custom_components/food_scanner/__init__.py
@@ -28,7 +28,7 @@ from .units import async_install_standard_units
 RUNTIME_KEY = f"{DOMAIN}_runtime"
 PANEL_URL_PATH = "food-scanner"
 PANEL_STATIC_URL = "/food_scanner_static"
-PANEL_VERSION = "1.6.3"
+PANEL_VERSION = "1.6.5"
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     return True
