a0a90b03e168f5ca483bcf741765e97b04f9892f
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-09-06 14:07:23 +0200

    HomeStock 1.6.19: load safe-area and scan UI fixes

--- custom_components/food_scanner/www/panel_boot.js
+++ custom_components/food_scanner/www/panel_boot.js
@@ -1,8 +1,8 @@
-import "/food_scanner_static/panel_v167.js?v=1.6.18";
+import "/food_scanner_static/panel_v168.js?v=1.6.19";
 
 const Panel = customElements.get("food-scanner-panel");
 if (Panel) {
-  Panel.prototype.__homestock_frontend_build = "1.6.18";
+  Panel.prototype.__homestock_frontend_build = "1.6.19";
   Panel.prototype._download = async function(format) {
     try {
       const data = await this._hass.callApi(
