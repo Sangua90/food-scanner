17c08b571205135ee53f74c77e9304343ad27b59
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-09-06 11:59:20 +0200

    Set HomeStock panel version 1.6.12

--- custom_components/food_scanner/__init__.py
+++ custom_components/food_scanner/__init__.py
@@ -28,7 +28,7 @@ from .units import async_install_standard_units
 RUNTIME_KEY = f"{DOMAIN}_runtime"
 PANEL_URL_PATH = "food-scanner"
 PANEL_STATIC_URL = "/food_scanner_static"
-PANEL_VERSION = "1.6.11"
+PANEL_VERSION = "1.6.12"
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     return True
