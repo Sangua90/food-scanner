c527ee15752f9c11d18f43794eef38cc6dbb6bee
Sangua90 <93098330+Sangua90@users.noreply.github.com>
2026-09-06 19:12:42 +0200

    HomeStock 1.6.36 frontend

--- custom_components/food_scanner/www/panel_boot.js
+++ custom_components/food_scanner/www/panel_boot.js
@@ -1,8 +1,8 @@
-import "/food_scanner_static/panel_v180.js?v=1.6.35";
+import "/food_scanner_static/panel_v181.js?v=1.6.36";
 
 const Panel = customElements.get("food-scanner-panel");
 if (Panel) {
-  Panel.prototype.__homestock_frontend_build = "1.6.35";
+  Panel.prototype.__homestock_frontend_build = "1.6.36";
   Panel.prototype._download = async function(format) {
     try {
       const data = await this._hass.callApi(
